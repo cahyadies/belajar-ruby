fe0ec65507adb2211001fa557457b2df0c8c041a
Eko Sugeng Cahyadi <ekosugengcahyadi@gmail.com>
2021-10-11 15:58:50 +0700

    menambahkan sedikit baris code pada file input

--- 7.input.rb
+++ 7.input.rb
@@ -1,7 +1,18 @@
 #input menggunakan getChomp
 
-puts "Berapa umur kamu ?"
-
-umur = gets.chomp
+print "Umur om berapa  ?"
+umur = gets.chomp 
 
 puts "Jadi umur saya #{umur}"
+
+print "Mantannya berapa om ?"
+mantan = gets.chomp
+
+puts "Mantan saya tuh #{mantan}"
+
+#======================================
+
+puts "Masukan angka pertama: "
+a = gets.chomp
+puts "Masukan angka kedua"
+b = gets.chomp
